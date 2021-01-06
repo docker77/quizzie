0b3f9b9fe9e2cd6477fb163c2a9924afa8c52bf3
Rustam Ababakirov <docker123@gmail.com>
2021-01-06 18:06:13 +0500

    Bootstrap for react added

--- plugins/flow-constructor/admin/src/containers/HomePage/index.js
+++ plugins/flow-constructor/admin/src/containers/HomePage/index.js
@@ -6,31 +6,50 @@
 
 import React, { memo } from "react";
 // import PropTypes from 'prop-types';
-import pluginId from "../../pluginId";
-import Accordion from "../../components/accordion";
-import Example from "../../components/example";
+import { Card, Button, Accordion } from "react-bootstrap";
 
 const HomePage = () => {
   return (
     <div className="container pt-3">
       <div className="row">
-        <div
-          className="col-6 pt-3"
-          style={{
-            marginBottom: "1.6rem",
-            fontSize: "1.3rem",
-            fontFamily: "Lato",
-          }}
-        >
-          <Accordion />
+        <div className="col-3">
+          <Card style={{ width: "100%" }}>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary" className="py-2 px-4">
+                Go somewhere
+              </Button>
+            </Card.Body>
+          </Card>
         </div>
-        <div className="col-6">
-          <h1>Test rows</h1>
+        <div className="col-9">
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  Click me!
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>Hello! I'm the body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                  Click me!
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>Hello! I'm another body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-6"></div>
-        <div className="col-6">Test cols</div>
       </div>
     </div>
   );
